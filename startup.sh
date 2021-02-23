ca9145c1c5194239ce20d35ad80cd9ebecd92463
rkochhan <rkochhan@student.42sp.org.br>
2021-02-22 21:41:04 -0300

    sublime files, scripts, and aliases

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-mount | grep -q "$HOME/Documents/gdrive/gdrive" || google-drive-ocamlfuse "$HOME/Documents/gdrive/gdrive"
+# mount | grep -q "$HOME/Documents/gdrive/gdrive" || google-drive-ocamlfuse "$HOME/Documents/gdrive/gdrive"
+
 imwheel -k &>/dev/null
+
+#sleep 3
+#nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceCompositionPipeline=On}"
