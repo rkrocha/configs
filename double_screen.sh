aa08cf6f06e5c17183d7822ed74e7fcaaf8295e4
rkrocha <rafaelkr7@gmail.com>
2022-07-16 16:30:09 -0300

    add double monitor setup scropts

---
+++ double_screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --mode 1920x1080 --pos 0x0 --rate 144 --rotate left --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 1080x390 --rate 144 --rotate normal --output DP-3 --off --output HDMI-0 --off --output DP-4 --off --output DP-5 --off
